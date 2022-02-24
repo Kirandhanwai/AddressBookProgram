8aed615e1ab0a533a72553813cff3ec9b3fc7895
Kirandhanwai <kirandhanwai123@gmail.com>
2022-02-25 00:10:24 +0530

    Added :New Contact to Adress Book

--- src/com/blz/java/AddressBook.java
+++ src/com/blz/java/AddressBook.java
@@ -1,10 +1,51 @@
 package com.blz.java;
 
+import java.util.ArrayList;
+
+import java.util.Scanner;
+
 public class AddressBook {
+		public static void main(String[]Args) {
+			ArrayList<Contact> array = new ArrayList<Contact>();
+			try (Scanner sc = new Scanner(System.in)) {
+				try (Scanner sc1 = new Scanner(System.in)) {
+					int choice;
+					do {
+						System.out.println("1.Adding New Contact");
+						System.out.println("2.Check Added Contact");
+						System.out.println("Enter your choice");
+						choice = sc.nextInt();
 
-	public static void main(String[] args) {
-	System.out.println("Welcome to addressBook java Program");
-
-	}
+						switch (choice) {
+						case 1:
+							System.out.print("Enter First Name: ");
+							String firstName = sc1.nextLine();
+							System.out.print("Enter last Name: ");
+							String lastName = sc1.nextLine();
+							System.out.print("Enter your address: ");
+							String address = sc1.nextLine();
+							System.out.print("Enter your city: ");
+							String city = sc1.nextLine();
+							System.out.print("Enter your state: ");
+							String state = sc1.nextLine();
+							System.out.print("Enter zip code : ");
+							int zipcode = sc.nextInt();
+							System.out.print("Enter phone number: ");
+							String phoneNo = sc1.nextLine();
+							System.out.print("Enter your email: ");
+							String email = sc1.nextLine();
+							Contact contact = new Contact(firstName, lastName, address, city, zipcode, state, phoneNo,
+									email);
+							array.add(contact);
+							break;
+						case 2:
+							System.out.println(array);
+							break;
+						}
+					} while (choice != 0);
+					System.out.println("Invalid Choice!!!");
+				}
+			}
+		}
 
 }
