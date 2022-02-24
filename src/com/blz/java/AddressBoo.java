f50c1d884b51b7d243b17831609465065f3c4c6d
Kirandhanwai <kirandhanwai123@gmail.com>
2022-02-25 00:00:48 +0530

    Added :Creat Contact

--- src/com/blz/java/AddressBoo.java
+++
@@ -1,8 +0,0 @@
-package com.blz.java;
-
-public class AddressBoo {
-	public static void main (String[] args) {
-		System.out.println("Welcome AddressBook Program");
-	}
-
-}
---
+++ src/com/blz/java/AddressBook.java
@@ -0,0 +1,10 @@
+package com.blz.java;
+
+public class AddressBook {
+
+	public static void main(String[] args) {
+	System.out.println("Welcome to addressBook java Program");
+
+	}
+
+}
